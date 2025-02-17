2a243cfbdf4032ae69a1bd755998ee4981925cc5
Daniel Morgan <me@danielmorgan.co.uk>
2025-02-17 23:20:39 +0000

    Start following galaxies.dev zustand tut

--- app/(app)/(paired)/_layout.tsx
+++ app/(app)/(paired)/_layout.tsx
@@ -1,14 +1,62 @@
 import { PairPresenceProvider } from "@/ctx/PairPresenceContext";
-import { Tabs } from "expo-router";
-import { View } from "react-native";
+import { Link, Tabs } from "expo-router";
+import { Pressable, View } from "react-native";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 export default function PairedLayout() {
   return (
     <PairPresenceProvider>
       <View style={{ flex: 1 }}>
-        <Tabs screenOptions={{ headerShown: false }}>
-          <Tabs.Screen name="index" options={{ title: "Home" }} />
-          <Tabs.Screen name="settings" options={{ title: "Settings" }} />
+        <Tabs>
+          <Tabs.Screen
+            name="index"
+            options={{
+              title: "Power",
+              tabBarIcon: ({ color }) => (
+                <FontAwesome size={24} name="bolt" color={color} />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="products"
+            options={{
+              title: "Products",
+              tabBarIcon: ({ color }) => (
+                <FontAwesome size={24} name="shopping-bag" color={color} />
+              ),
+              headerRight: () => (
+                <Link href="/(app)/(paired)/cart" asChild>
+                  <Pressable style={{ marginRight: 16 }}>
+                    {({ pressed }) => (
+                      <FontAwesome
+                        name="shopping-cart"
+                        size={24}
+                        color={pressed ? "#999" : "#000"}
+                      />
+                    )}
+                  </Pressable>
+                </Link>
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="cart"
+            options={{
+              title: "Cart",
+              tabBarIcon: ({ color }) => (
+                <FontAwesome size={24} name="shopping-cart" color={color} />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="settings"
+            options={{
+              title: "Settings",
+              tabBarIcon: ({ color }) => (
+                <FontAwesome size={24} name="cog" color={color} />
+              ),
+            }}
+          />
         </Tabs>
       </View>
     </PairPresenceProvider>
